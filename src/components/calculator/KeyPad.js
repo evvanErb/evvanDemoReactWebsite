ba318290d2a68484ebf93a9768333731e7256686
evvanErb <evvan@protonmail.com>
2021-02-14 22:24:23 -0500

    keypad font size

--- src/components/calculator/KeyPad.js
+++ src/components/calculator/KeyPad.js
@@ -10,69 +10,69 @@ class KeyPad extends Component {
                         <button name="%" class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >%</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button name="CE" class="btn btn-dark" style={{ height: "75px", width: "75px" }} >CE</button>
+                        <button name="CE" class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >CE</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >+/-</button>
+                        <button name="+/-" class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >+/-</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >C</button>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >1</button>
-                    </div>
-                    <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >2</button>
-                    </div>
-                    <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >3</button>
-                    </div>
-                    <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >+</button>
+                        <button name="C" class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >C</button>
                     </div>
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >4</button>
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >1</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >5</button>
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >2</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >6</button>
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >3</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >-</button>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >7</button>
-                    </div>
-                    <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >8</button>
-                    </div>
-                    <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >9</button>
-                    </div>
-                    <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >*</button>
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >+</button>
                     </div>
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >.</button>
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >4</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >0</button>
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >5</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >=</button>
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >6</button>
                     </div>
                     <div class="col-md-1 mb-4">
-                        <button class="btn btn-dark" style={{ height: "75px", width: "75px" }} >/</button>
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >-</button>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-1 mb-4">
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >7</button>
+                    </div>
+                    <div class="col-md-1 mb-4">
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >8</button>
+                    </div>
+                    <div class="col-md-1 mb-4">
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >9</button>
+                    </div>
+                    <div class="col-md-1 mb-4">
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >*</button>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-1 mb-4">
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >.</button>
+                    </div>
+                    <div class="col-md-1 mb-4">
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >0</button>
+                    </div>
+                    <div class="col-md-1 mb-4">
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >=</button>
+                    </div>
+                    <div class="col-md-1 mb-4">
+                        <button class="btn btn-dark" style={{ fontSize: "30px", height: "75px", width: "75px" }} >/</button>
                     </div>
                 </div>
             </div>
