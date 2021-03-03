49abcf96e3aa55f879425ebb440e1b7527e3d016
evvanErb <evvan@protonmail.com>
2021-03-03 17:47:09 -0500

    added skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,7 +14,7 @@ class Skills extends Component {
                         <p style={{ fontSize: "30px" }} >Git, GitHub, Django, Unity, React, Chrome Extensions, Atom, VS Code, Emacs, Linux, Windows</p>
                       </div>
                       <div className="col-md-3 mb-4" >
-                        <p style={{ fontSize: "30px" }} >Python, Java, Javascript, HTML, OCaml, C, Groovy, C++, C#, CI/CD, Jenkins, JIRA, Confluence, Agile, MySQL</p>
+                        <p style={{ fontSize: "30px" }} >Python, Java, TypeScript, JavaScript, HTML, OCaml, C, Groovy, C++, C#, CI/CD, Jenkins, JIRA, Confluence, Agile, MySQL</p>
                       </div>
                       <div className="col-md-3 mb-4" >
                         <p style={{ fontSize: "30px" }} >Microsoft Office Suite, Google Suite, Kali Linux / Pentesting, Brewing, French</p>
