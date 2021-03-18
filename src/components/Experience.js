ea1f25c4ab17e3ef9e499239f97d87d34aea7eaa
evvanErb <evvan@protonmail.com>
2021-03-18 15:59:58 -0400

    Added UMass Lowell

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,6 +9,7 @@ class Experience extends Component {
               <h1 style={{ fontSize: "50px" }}>Experience</h1>
               <span className="lead" style={{ fontSize: "25px" }} >
                   <br /><br />
+                  <h3>University of Massachusetts, Lowell</h3><i>Master of Business Administration</i>. Jan 2022 - Current. GPA : N/A<br /> <br />
                   <h3>University of Maryland, College Park</h3><i>B.S. Computer Science, Business (Entrepreneurship/Innovation) Minor</i>. Aug 2018 - Dec 2021. GPA : 3.42<br /> <br />
                   <h3>Populus Media, New York, NY</h3><i>Software Development Associate</i>. Jan 2021 - Current<br /> <br />
                   <h3>Vertex Inc., King of Prussia, PA</h3><i>Software Development Intern</i>. Jun 2020 - Aug 2020, Jun 2021 - Aug 2021<br /> <br />
